19869fcf859d2dbd1e86301914596f90452d1777
Ashwani <ashwanikaushik133@gmail.com>
2024-12-04 23:04:40 +0530

    remove unwanted images/add responsiveness

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -14,16 +14,16 @@ export function Cart(){
     const location = useLocation();
 
 
-    return <div className="h-[100vh] sticky top-0 z-100 relative flex justify-center items-center">
-    <div className="h-[85%] w-[85%] bg-[hsl(18.12_99.02%_60%)] shadow-[0px_10px_30px_5px_hsl(214.29_17.41%_40%)] rounded-lg p-10">
-        <div className='flex justify-between'>
+    return <div className="h-[100vh] sticky top-0 z-100 relative flex justify-center items-center bg-white">
+    <div className="h-[85%] w-[90%] sm:w-[85%] bg-[hsl(18.12_99.02%_60%)] shadow-[0px_10px_30px_5px_hsl(214.29_17.41%_40%)] rounded-lg p-10">
+        <div className='flex justify-between items-center flex-wrap gap-2'>
         <div className={`font-dancingScript text-2xl font-extrabold
         ${location.pathname === '/cart'?'inline p-1 cursor-pointer active:scale-95':'cursor-text'}`}><Link className={`${location.pathname === '/cart'?'cursor-pointer':'cursor-text'}`} to='/'>Amazing cart</Link></div>
         <div className={`p-1 cursor-pointer active:scale-95 flex justify-center items-center`}><Link to='/wishlist'>Wishlist</Link></div>
         </div>
-        <p className='p-3 font-poppins tracking-tight font-bold'>Your shopping list</p>
-        <div className="flex gap-5">
-            <div className="flex-[3] rounded gap-3 overflow-auto max-h-[400px]">
+        <p className='p-3 font-poppins tracking-tight font-bold sm:text-left'>Your shopping list</p>
+        <div className="flex flex-col md:flex-row gap-5">
+            <div className="flex-[3] rounded gap-3 overflow-auto max-h-[220px] md:max-h-[400px]">
                 {cartItemState.map((item,index) => <ItemsInCart key={index} image={item.image} name={item.name} quantity={item.quantity} price={item.price} id={item.id}/>)}
             </div>
             <div className="flex-[1] p-4 shadow-xl block rounded text-black">
@@ -37,7 +37,7 @@ export function Cart(){
                     <div><FontAwesomeIcon icon={faIndianRupeeSign} className='mr-1'/>{total}</div>
                 </div>
                 <div className='flex justify-center'>
-                <button className='p-2 px-10 bg-black font-bold text-primary rounded  mt-4 shadow-xl active:scale-95'>Proceed to Buy</button>
+                <button className='p-2 px-6 sm:px-10 bg-black font-bold text-primary rounded  mt-4 shadow-xl active:scale-95'>Proceed to Buy</button>
                 </div>
             </div>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,9 @@
 import '../assets/hero.css'
-import orangeCart from '../assets/images/orangeCart-Photoroom.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 import {Link} from 'react-router-dom'
 import hero from '../assets/images/hero.png'
 import Headphones from '../assets/images/Headphones .png'
 import Game from '../assets/images/Vintage Handheld Game.png'
-import Console from '../assets/images/Portable Gaming Device.png'
-import Earpods from '../assets/images/Earpods.png'
-import Mic from '../assets/images/Sleek Modern Handheld Microphone .png'
 import hero1 from '../assets/images/Contemplative City Night.jpeg'
-import hero2 from '../assets/images/Stylized Character with Red Electric Guitar.jpeg'
-import hero3 from '../assets/images/Joyful Pose with Headphones.jpeg'
-import hero4 from '../assets/images/Modern Digital Communication.jpeg'
 import { useEffect, useState } from 'react';
 
 
@@ -27,21 +18,21 @@ export function Hero(){
         },2000)
 
         return ()=> clearInterval(id)
-    },2000)
+    },[])
 
     return <div className='w-[90%]'>
-        <div className='flex justify-between items-center'>
-        <div className='text-black '>
-            <div className='lg:text-6xl md:text-6xl font-cinzel font-semibold tracking-tight tt-1'>shop smart</div>
-            <div className='lg:text-6xl md:text-6xl font-cinzel font-semibold tracking-tight text-left tt-2'>& live <span className='text-secondary font-ptSerif font-medium uppercase'>fashionably</span></div>
-            <div className='lg:text-6xl md:text-6xl font-medium tracking-tight text-secondary font-ptSerif uppercase tt-3'> better <span className='text-black font-cinzel font-semibold'>everyday</span></div>
-            <Link to='/wishlist'><button className='px-16 py-4 bg-[hsl(18.12_99.02%_60%)] text-xl font-bold text-white rounded mt-10 shadow-xl active:scale-95 font-ptSerif uppercase tracking-widest'>Shop now</button></Link>
+        <div className='flex flex-col lg:flex-row justify-between items-center'>
+        <div className='text-black text-center md:text-left'>
+            <div className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-cinzel font-semibold tracking-tight tt-1'>shop smart</div>
+            <div className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-cinzel font-semibold tracking-tight text-left tt-2'>& live <span className='text-secondary font-ptSerif font-medium uppercase'>fashionably</span></div>
+            <div className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-medium tracking-tight text-secondary font-ptSerif uppercase tt-3'> better <span className='text-black font-cinzel font-semibold'>everyday</span></div>
+            <Link to='/wishlist'><button className='px-10 sm:px-12 md:px-16 py-2 sm:py-3 md:py-4 bg-[hsl(18.12_99.02%_60%)] text-lg sm:text-xl font-bold text-white rounded mt-6 md:mt-10 shadow-xl active:scale-95 font-ptSerif uppercase tracking-widest'>Shop now</button></Link>
             <img src={Headphones} alt="image" className='absolute w-[12%] top-24 opacity-0 tt-icon1'/>
             <img src={Game} alt="" className='absolute w-[12%] bottom-20 right-20 opacity-0 tt-icon2'/>
         </div>
-        <div className='w-[40%] tt-img relative'>
+        <div className='w-[100%] lg:w-[40%] tt-img relative mt-6 md:mt-0'>
             <img src={hero} alt="image" className=''/>
-            <img src={hero1} alt="" className={`absolute bottom-4 left-28 w-[48%] h-[91%] rounded-xl ${showImage?'heroImageUnhide':'heroImageHide'}`}/>
+            <img src={hero1} alt="" className={`absolute bottom-2 left-16  md:left-40 md:bottom-6 lg:bottom-4 lg:left-16 xl:bottom-4 xl:left-28 w-[48%] h-[91%] rounded-xl ${showImage?'heroImageUnhide':'heroImageHide'}`}/>
         </div>
         </div>
     </div>
